03731dda46965b3cad68cf366c708affac5563ff
Ilya Ayana <77210943+ilyaayana@users.noreply.github.com>
2023-02-28 14:22:31 +0000

    added exception

--- src/main.c
+++ src/main.c
@@ -14,17 +14,27 @@ int countLetter(char letter, char* sentence)
     return number;
 }
 
-void main(int argc, char* argv[])
+int main(int argc, char* argv[])
 {
-
+    if(argc <3)
+    {
+      printf("Error: not enough arguments");
+      return -1;
+    }
     char* letter = argv[1];
 
+    printf("the letter: %c",*letter);
+  
+    printf("\nsentences:\n");
     int totalNumber = 0;
     for (int i = 2; i < argc; i++)
     {
+        printf("%s\n",argv[i]);
         totalNumber += countLetter(*letter, argv[i]);
     }
 
-    printf("The total of number of occurences of letter %c is %d", *letter, totalNumber);
+    printf("\nThe total of number of occurences of letter %c is %d", *letter, totalNumber);
+
+    return 0;
 
 }
