2cad8e38bbc8b43313b5b0240b5d5e788d8a38cd
Ilya Ayana <77210943+ilyaayana@users.noreply.github.com>
2023-02-28 14:04:22 +0000

    modified ReadMe, added countLetter function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,30 @@
-/* hello.c */
 #include <stdio.h>
+#include <string.h>
 
-void main (void)
+int countLetter(char letter, char* sentence)
 {
-	printf ("Hello World\n");
-	printf ("Goodbye World\n");
+    int number = 0;
+    for (int i = 0; i < strlen(sentence); i++)
+    {
+        if (sentence[i] == letter)
+        {
+            number++;
+        }
+    }
+    return number;
+}
+
+void main(int argc, char* argv[])
+{
+
+    char* letter = argv[1];
+
+    int totalNumber = 0;
+    for (int i = 2; i < argc; i++)
+    {
+        totalNumber += countLetter(*letter, argv[i]);
+    }
+
+    printf("The total of number of occurences of letter %c is %d", *letter, totalNumber);
+
 }
